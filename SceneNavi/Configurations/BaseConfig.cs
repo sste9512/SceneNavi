f6e2473c48840d996b240bd4ebfd4384c867cc00
SteLink <steven.stewart9512@gmail.com>
2020-11-30 13:03:16 -0800

    Refactored (wip)

---
+++ SceneNavi/Configurations/BaseConfig.cs
@@ -0,0 +1,29 @@
+﻿namespace SceneNavi.Configurations
+{
+    public interface IBaseConfig
+    {
+        string UpdateServer { get; set; }
+        bool RenderRoomActors { get; set; }
+        bool RenderSpawnPoints { get; set; }
+        bool RenderTransitions { get; set; }
+        string LastRom { get; set; }
+        bool RenderPathWaypoints { get; set; }
+        bool LinkAllWPinPath { get; set; }
+        bool RenderTextures { get; set; }
+        bool RenderCollision { get; set; }
+        bool RenderCollisionAsWhite { get; set; }
+        bool OglvSync { get; set; }
+        ToolModes LastToolMode { get; set; }
+        string LastSceneFile { get; set; }
+        string LastRoomFile { get; set; }
+        CombinerTypes CombinerType { get; set; }
+        bool ShownExtensionWarning { get; set; }
+        bool ShownIntelWarning { get; set; }
+        bool RenderWaterboxes { get; set; }
+        bool ShowWaterboxesPerRoom { get; set; }
+        bool IsRestarting { get; set; }
+        int AntiAliasingSamples { get; set; }
+        bool EnableAntiAliasing { get; set; }
+        bool EnableMipmaps { get; set; }
+    }
+}
---
+++ SceneNavi/RomHandlers/MajorasMaskRomHandler.cs
@@ -0,0 +1,68 @@
+﻿using SceneNavi.ROMHandler;
+using SceneNavi.ROMHandler.Interfaces;
+
+namespace SceneNavi.RomHandlers
+{
+    public class MajorasMaskRomHandler : IRomHandler
+    {
+        public void ReadRomHeader()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ReadEntranceTable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ReadDmaTable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ReadFileNameTable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FindBuildInfo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FindFileNameTable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FindCodeFile()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FindSceneTable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FindActorTable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FindObjectTable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsAddressSupported(uint address)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DetectByteOrder()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
